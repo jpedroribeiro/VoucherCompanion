4776d500b1655deb0bdc2c0671d975aa62920b5c
Pedro Ribeiro <pribeiro@rmn.com>
2017-02-03 16:05:01 +0000

    Adding not found state and better UI

--- background/index.js
+++ background/index.js
@@ -8,37 +8,56 @@ chrome.runtime.onMessage.addListener(function (msg, sender) {
 	JSONreq.onreadystatechange = function() {
 		if (JSONreq.readyState == 4 && JSONreq.status == 200) {
 			var match = isAMatch(msg.merchant, JSON.parse(JSONreq.responseText));
-			console.log(match);
-			// Respond with a nice template
-			var req = new XMLHttpRequest();
-			req.open("GET", chrome.extension.getURL('background/index.html'), true);
-			req.onreadystatechange = function() {
-				if (req.readyState == 4 && req.status == 200) {
-					var html = req.responseText
-								.replace('__MERCHANT_NAME', match.name)
-								.replace('__OFFER', match.offers[0])
-								.replace('__MERCHANT_URL', msg.merchant);
-					
-					chrome.tabs.sendMessage(sender.tab.id, {
-						markup: html
-					});
-				}
-			};
-			req.send(null);
-			// end template xhr
+			
+			
+			if (match.offers) {
+				// Offer found
+				var req = new XMLHttpRequest();
+				req.open("GET", chrome.extension.getURL('background/found.html'), true);
+				req.onreadystatechange = function() {
+					if (req.readyState == 4 && req.status == 200) {
+						var html = req.responseText
+							.replace('__MERCHANT_NAME', match.name)
+							.replace('__OFFER', match.offers[0])
+							.replace('__MERCHANT_URL', msg.merchant);
+						
+						chrome.tabs.sendMessage(sender.tab.id, {
+							markup: html
+						});
+					}
+				};
+				req.send(null);
+				
+				// TODO this could be the number of offers (OR change the icon)
+				// chrome.browserAction.setBadgeBackgroundColor({color:[0, 0, 0]});
+				// chrome.browserAction.setBadgeText({text:"0"});
+			} else {
+				// Nothing found
+				var req = new XMLHttpRequest();
+				req.open("GET", chrome.extension.getURL('background/notfound.html'), true);
+				req.onreadystatechange = function() {
+					if (req.readyState == 4 && req.status == 200) {
+						var html = req.responseText
+							.replace('__MERCHANT_NAME', match.name)
+							.replace('__SIMILAR_NAME', match.similar)
+							.replace('__SIMILAR_OFFER', match.similarOffers[0])
+							.replace('__SIMILAR_URL', match.similarURL);
+						
+						chrome.tabs.sendMessage(sender.tab.id, {
+							markup: html
+						});
+					}
+				};
+				req.send(null);
+				
+				// TODO this could be the number of offers (OR change the icon)
+				// chrome.browserAction.setBadgeBackgroundColor({color:[0, 0, 0]});
+				// chrome.browserAction.setBadgeText({text:"0"});
+			}
 		}
 	};
 	JSONreq.send(null);
 	// end json xhr
-	
-	
-	
-	// TODO this could be the number of offers (OR change the icon)
-	// chrome.browserAction.setBadgeBackgroundColor({color:[0, 0, 0]});
-	// chrome.browserAction.setBadgeText({text:"0"});
-	
-	
-	
 });
 
 
@@ -49,8 +68,11 @@ function isAMatch(merchant, obj) {
 	for (var prop in obj) {
 		if (obj[prop].url === merchant) {
 			match = {
-				offers: obj[prop].offers,
-				name: prop
+				offers: obj[prop].offers.length > 0 ? obj[prop].offers : false,
+				name: prop,
+				similarOffers: obj[prop].similar ? obj[obj[prop].similar].offers : false,
+				similar: obj[prop].similar ? obj[prop].similar : false,
+				similarURL: obj[prop].similar ? obj[obj[prop].similar].url : false
 			};
 		}
 	}
--- injector/index.js
+++ injector/index.js
@@ -7,8 +7,11 @@ chrome.runtime.sendMessage({
 chrome.runtime.onMessage.addListener(function(msg, sender, response){
 	document.querySelector('body').insertAdjacentHTML('beforeend', msg.markup);
 	
+	setTimeout(function(){
+		document.querySelector('.voucher-companion-container').style.opacity = 1;
+		document.querySelector('.voucher-companion-container').style.transform = 'initial';
+	}, 1000);
 	
-	document.querySelector('.voucher-companion-container').style.opacity = 1;
 	
 	
 	
